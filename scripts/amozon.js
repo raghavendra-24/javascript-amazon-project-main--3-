decd236542149012ba8656e9f841d136c1ee7541
janaki ram <janakiram@gmail.com>
2024-03-05 16:43:06 +0530

    Add the starting files

--- scripts/amozon.js
+++ scripts/amozon.js
@@ -1,33 +1,7 @@
-const products=[{
-    image:"images/products/athletic-cotton-socks-6-pairs.jpg",
-    name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating:{
-        stars:4.5,
-        count:87,
-        starsImage:"images/ratings/rating-45.png"
-    },
-    priceCents:1090,
-
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating:{
-        stars:4,
-        count:127,
-        starsImage:'images/ratings/rating-40.png'
-    },
-    priceCents:2095,
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating:{
-        stars:4.5,
-        count:56,
-        starsImage:'images/ratings/rating-45.png'
-    },
-    priceCents:799,
-}]
+    import {cart} from '../data/cart.js';
+    
     let productsHtml=``;
+
 
 products.forEach((product)=>{
      productsHtml+=`
@@ -75,7 +49,7 @@ products.forEach((product)=>{
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>`
@@ -84,3 +58,34 @@ products.forEach((product)=>{
 console.log(productsHtml);
 
 document.querySelector(".js-products-grid").innerHTML=productsHtml
+
+document.querySelectorAll(".js-add-to-cart-button").forEach((button)=>{
+  button.addEventListener('click',()=>{
+    let productid=button.dataset.productId;
+
+    let matchingItem;
+    cart.forEach((item)=>{
+      if(item.productId===productid){
+          matchingItem=item;
+      }
+    })
+
+    if(matchingItem){
+      matchingItem.quantity++;
+    }else{
+    cart.push({
+      productId:productid,
+      quantity:1,
+    });
+  }
+    
+
+    let cartQuantity=0;
+    cart.forEach((item)=>{
+      cartQuantity+=item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML=cartQuantity;
+   
+  });
+});
