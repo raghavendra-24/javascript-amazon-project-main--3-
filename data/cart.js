9e7ac4c47e3f07030ce483990324b3f2dd8ee6fb
janaki ram <janakiram@gmail.com>
2024-03-05 21:11:18 +0530

    modified

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,23 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart=
+[{
+
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2,
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:3,
+},{
+  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity:3,
 }];
+}
+
+function saveToStorage(){
+localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productid){
     let matchingItem;
@@ -22,4 +35,17 @@ export function addToCart(productid){
       quantity:1,
     });
   }
+  saveToStorage();
+  }
+
+
+  export function removeFromCart(productId){
+      const newCart=[];
+      cart.forEach((item)=>{
+          if(item.productId!=productId){
+            newCart.push(item);
+          }
+      });
+      cart=newCart;
+      saveToStorage();
   }
